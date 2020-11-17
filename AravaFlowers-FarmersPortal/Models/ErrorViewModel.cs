0e0f2c70c83e616ef697af4e12dfc57e1ee40460
David Kulpe <davidkulpe@gmail.com>
2020-11-17 15:53:38 +0200

    First of his kind

---
+++ AravaFlowers-FarmersPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AravaFlowers_FarmersPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
